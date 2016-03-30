f9327b2a541c6aa87edaf4bbf15599915e20e5b0
Wilson Ndirangu <wndirangu@cytonn.com>
2016-03-30 18:04:22 +0300

    modify function

--- js/functions.js
+++ js/functions.js
@@ -4,18 +4,34 @@
  *
  */
 
- var app=angular.module('YahooCurrencies', []);
+var app = angular.module('NetflixMovies', []);
 
- app.controller('retrieveCurrenciesController', ['$scope','$http', function($scope,$http) {
-   $scope.items = [];
+app.controller('retrieveMovieController', ['$scope', '$http', function($scope, $http) {
+  $scope.items = [];
+  var self = this;
 
-   $scope.getItems = function() {
-    $http({method: 'GET',url: 'http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=jsonp'})
-       .success(function(data, status) {
-           $scope.items = data;
-        })
-       .error(function(data, status) {
-           alert("Error");
-       });
-   };
+  var Moviename = String(self.moviename);
+
+  $scope.getItems = function() {
+    var split_string = self.moviename.split(" ");
+    length = split_string.length;
+    var api = "http://netflixroulette.net/api/api.php?title=" + split_string[0];
+    for (i = 1; i < length; i++) {
+      api += "%20" + split_string[i];
+    }
+    $http({
+        method: 'GET',
+        url: api
+      })
+      .success(function(data, status) {
+        $scope.items = data;
+
+        console.log(split_string[0]);
+
+
+      })
+      .error(function(data, status) {
+        alert(" Sorry We couldn't find a movie with that title!");
+      });
+  };
 }]);
